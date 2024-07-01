61f0e21b88040e085a9a61b117afa9879618fc9d
MuhamedOtajagic <otajagicmuhamed@gmail.com>
2024-07-01 17:51:23 -0400

    prerender test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
 
 export const routes: Routes = [{
-    // path: '', component: AppComponent
+    path: 'users', component: UsersComponent
 }];
