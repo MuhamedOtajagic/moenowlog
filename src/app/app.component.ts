f9d4e38af1a389df4d99e690abce11df068d24bf
MuhamedOtajagic <otajagicmuhamed@gmail.com>
2024-07-03 14:17:26 -0400

    centered contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
