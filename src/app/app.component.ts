2a9b75cb73cfe785c4bfe423121d2ef7be8acf76
MuhamedOtajagic <otajagicmuhamed@gmail.com>
2024-07-03 19:43:41 -0400

    partially mobile responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
+import { BreakpointObserver, Breakpoints, LayoutModule } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, ContactComponent],
+  imports: [RouterOutlet, RouterModule, ContactComponent, LayoutModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'moenowblog';
+  
+  isPhonePortrait = false;
+  isTabletLandscape = false;
+  isTabletPortrait = false
+
+  constructor(private responsive:BreakpointObserver){
+
+  }
+
+  ngOnInit() {
+    this.responsive.observe([
+      Breakpoints.TabletLandscape,
+      Breakpoints.TabletPortrait,
+      Breakpoints.HandsetPortrait
+    ])
+      .subscribe(result => {
+        this.isTabletLandscape = false;
+        this.isPhonePortrait = false;
+  
+        if (result.breakpoints[Breakpoints.TabletLandscape]) {
+          this.isTabletLandscape = true;
+        }
+        if (result.breakpoints[Breakpoints.TabletPortrait]) {
+          this.isTabletPortrait = true;
+        }
+        if (result.breakpoints[Breakpoints.HandsetPortrait]) {
+          this.isPhonePortrait = true;
+        }
+      });
+  }
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, LayoutModule } from '@angular/cdk/layout';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [LayoutModule, CommonModule, NgClass],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  isPhonePortrait = false;
+  isTabletLandscape = false;
+
+  constructor(private responsive:BreakpointObserver){
+
+  }
+
+  ngOnInit() {
+    this.responsive.observe([
+      Breakpoints.TabletLandscape,
+      Breakpoints.HandsetPortrait
+    ])
+      .subscribe(result => {
+        this.isTabletLandscape = false;
+        this.isPhonePortrait = false;
+  
+        if (result.breakpoints[Breakpoints.TabletLandscape]) {
+          this.isTabletLandscape = true;
+        }
+        if (result.breakpoints[Breakpoints.HandsetPortrait]) {
+          this.isPhonePortrait = true;
+        }
+      });
+  }
 
 }
